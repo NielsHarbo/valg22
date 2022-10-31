fb82c8f4539ea1ff0c52fc2bc61587ad5666cc4d
NielsHarbo <44673313+NielsHarbo@users.noreply.github.com>
2022-10-31 08:19:56 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,6 @@
+const data = [
+    ["A", "Soc.", 24.3, -1.6, "https://voxmeter.dk/wp-content/themes/ceminstitute2/images/A.png"],
+    ["B", "Rad.", 4.3, -4.3, "https://voxmeter.dk/wp-content/themes/ceminstitute2/images/B.png"],
+    ["C", "Kons.", 7, 0.4, "https://voxmeter.dk/wp-content/themes/ceminstitute2/images/C.png"],
+    ["D", "Nye B.", 4.9, 2.5, "https://voxmeter.dk/wp-content/themes/ceminstitute2/images/N.png"]
+]
